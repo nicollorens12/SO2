24dcae45a739878434bf48225beb50a14b0d8a08
ahels <mcvektor7@gmail.com>
2024-10-02 03:55:01 +0200

    Reajustar pila en RESTORE\n implementar sys_write

--- sys.c
+++ sys.c
@@ -45,3 +45,52 @@ int sys_fork()
 void sys_exit()
 {  
 }
+
+
+
+
+
+#define BUFFER_SIZE 512
+char buffer_content[BUFFER_SIZE];
+
+// fd: file descriptor. In this delivery it must always be 1.
+// buffer: pointer to the bytes.
+// size: number of bytes.
+// return ’ Negative number in case of error (specifying the kind of error) and
+// 			the number of bytes written if OK.
+int sys_write(int fd, char * buffer, int size){
+
+	// CHECK USER PARAMS
+	// Check fd (0: no hay error, < 0: id del error)
+	int id_error = check_fd(fd, ESCRIPTURA);
+	if(id_error < 0) return id_error;
+
+	// Check buffer 
+	if(buffer == NULL) return -1; // TODO - No se que devolver
+
+	// Check size
+	if(size < 0) return -2; // TODO - No se que devolver
+
+
+	
+	int bytes = size;
+	int bytes_written; 
+
+	while(bytes > BUFFER_SIZE){
+		// Copy data from user
+		copy_from_user(buffer, buffer_content, BUFFER_SIZE);
+		// Device routine --> write on console
+		bytes_written = sys_write_console(buffer_content, BUFFER_SIZE);
+		
+		buffer = buffer+BUFFER_SIZE;
+		bytes -= bytes_written;
+	}
+
+	// Imprimir los restantes
+	copy_from_user(buffer, buffer_content, bytes);
+	bytes_written = sys_write_console(buffer_content, bytes);
+	bytes -= bytes_written;	
+
+
+	return size - bytes;
+}
