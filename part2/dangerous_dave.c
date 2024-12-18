920b132fee94858f4d3830153f04e12502f27bbf
nicollorens12 <nicollorenss@gmail.com>
2024-12-18 19:25:51 +0100

    Game Working

--- part2/dangerous_dave.c
+++ part2/dangerous_dave.c
@@ -15,6 +15,8 @@ const char GEM[2] = {'*', BLACK << 4 | GREEN};
 const char PLAYER[2] = {'&', BLACK << 4 | LIGHT_BLUE};
 const char ENEMY[2] = {'+', BLACK << 4 | MAGENTA};
 const char GRAVITY_BOOST[2] = {'^', BLACK << 4 | LIGHT_RED};
+const char FINISH_DOOR[2] = {'=', LIGHT_CYAN << 4 | YELLOW};
+enum game_state {PLAYING, WIN, GAMEOVER, INIT};
 
 // Mapa del juego
 char map[NUM_ROWS][NUM_COLUMNS] = {
@@ -119,6 +121,7 @@ struct GameStatus {
     int score;
     int lives;
     int unlocked_platform;
+    enum game_state state;
 };
 
 struct Enemy enemies[2]; // Dos enemigos en las plataformas
@@ -128,16 +131,17 @@ struct Player player;
 struct GameStatus gameStatus;
 
 char key = 0; // Variable de tecla compartida
+int text_tearing = 0;
 struct sem_t *sem_key; // Semáforo para proteger la variable 'key'
 struct sem_t *sem_screen; // Semaforo para proteger escritura en pantalla
 float GRAVITY = 0.2f;
 
 void set_boosts(int reset){
     if(reset){
-        map[17][15] = ' ';
-        map[17][65] = ' ';
-        map[9][40]  = '$';
+        map[17][15] = '*';
+        map[17][65] = '*';  
     }
+    map[9][40]  = '$';
     map[13][25] = '^';
     map[13][55] = '^';
 
@@ -150,8 +154,9 @@ void init_game(){
     player.isJumping = 0;
 
     gameStatus.score = 0;
-    gameStatus.lives = 3;
+    gameStatus.lives = 1;
     gameStatus.unlocked_platform = 0;
+    lock_finish_slab();
 
     GRAVITY = 0.2f;
     set_boosts(1);
@@ -176,6 +181,12 @@ void keyboard_thread_func(void *param)
             key = c; // Actualizamos la tecla global de forma segura
             c = '\0'; // Limpiar la tecla
             semSignal(sem_key);
+        }
+        else if(gameStatus.state == GAMEOVER || gameStatus.state == WIN){
+            if(c == 'q'){
+                gameStatus.state = PLAYING;
+                init_game();
+            }
         }
     }
 }
@@ -233,6 +244,9 @@ void unlock_finish_slab(){
     map[8][74] = '#';
     map[8][75] = '#';
     map[8][76] = '#';
+
+    //Exit door
+    map[7][76] = '=';
 }
 void lock_finish_slab(){
     map[8][68] = ' ';
@@ -244,6 +258,7 @@ void lock_finish_slab(){
     map[8][74] = ' ';
     map[8][75] = ' ';
     map[8][76] = ' ';
+    map[7][76] = ' ';
 }
 
 
@@ -300,26 +315,25 @@ void update_player() {
     for (int i = 0; i < 2; i++) {
         if (player.p.x == enemies[i].p.x && player.p.y == enemies[i].p.y) {
             gameStatus.lives--; // Reducir vidas
-            reset_game();
-            if (gameStatus.lives > 0) {
-                map[player.p.y][player.p.x] = ' ';
-                player.p.x = 40;
-                player.p.y = 21;
-                player.velocityY = 0;
-                player.isJumping = 0;
-            }
+            if(gameStatus.lives > 0) reset_game();
+            else gameStatus.state = GAMEOVER;
             break;
         }
     }
 
     if (map[player.p.y][player.p.x] == '$') {
         gameStatus.score += 10;
+        gameStatus.unlocked_platform = 1;
     } 
     else if (map[player.p.y][player.p.x] == '*') {
         gameStatus.score += 5;
     }
     else if(map[player.p.y][player.p.x] == '^'){
         GRAVITY = 0.1f;
+    }
+    else if(map[player.p.y][player.p.x] == '='){
+        gameStatus.state = WIN;
+        //init_game();
     }
 
     map[player.p.y][player.p.x] = '&'; 
@@ -338,9 +352,15 @@ void update_thread_func(void *param)
 {
     while (1) 
     {
-        update_player();
-        update_enemies();
-        update_map();
+        if(gameStatus.state == PLAYING){
+            update_player();
+            update_enemies();
+            update_map();
+        }
+        
+        
+
+        
         // Pequeña pausa para no saturar la CPU
         int start_time = gettime();
         while (gettime() - start_time < 20); // Pausa de 20ms (REVISAR)
@@ -373,6 +393,9 @@ void render_map()
             } else if(map[i][j] == '^'){
                 render_map[i][j][0] = GRAVITY_BOOST[0];
                 render_map[i][j][1] = GRAVITY_BOOST[1];
+            } else if(map[i][j] == '='){
+                render_map[i][j][0] = FINISH_DOOR[0];
+                render_map[i][j][1] = FINISH_DOOR[1];
             } else {
                 render_map[i][j][0] = EMPTY[0];
                 render_map[i][j][1] = EMPTY[1];
@@ -427,7 +450,7 @@ void render_score_text()
     write(1, &buff, sizeof(buff));
 }
 
-int text_tearing = 0;
+
 void render_restart_text()
 {
     if (text_tearing < 10)
@@ -449,20 +472,24 @@ void render_thread_func(void *param)
     {
         int start_frame_time = gettime();
         
-        //if (gameStatus.lives > 0)
-        //{
-        //   render_map();
-        //   render_game_status();
-        //}
-        //else 
-        //{
+        if(gameStatus.state == PLAYING)
+        {
+           render_map();
+           render_game_status();
+        }
+        else if(gameStatus.state == WIN)
+        {
+            render_win_screen();
+            render_score_text();
+            render_restart_text();
+        }
+        else if(gameStatus.state == GAMEOVER)
+        {
             render_gameover_screen();
             render_restart_text();           
-        //}
-        {
-            //render_win_screen();
-            //render_score_text();
-            //render_restart_text();
+        }
+        else{
+            // Pantalla de inicio
         }
 
         while (gettime() - start_frame_time < TICKS_PER_FRAME);
